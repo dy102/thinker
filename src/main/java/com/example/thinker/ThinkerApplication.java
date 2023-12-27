f6bcba61138c4c9070aeba45000521e9f94f9390
dy102 <ekagnlek2@gmail.com>
2023-12-27 22:52:34 +0900

    Initial commit

---
+++ src/main/java/com/example/thinker/ThinkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.thinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thinker/ThinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
