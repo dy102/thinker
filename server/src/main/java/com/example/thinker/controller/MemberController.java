4ba718678b3a0e8d0b56419c0e18297d10256476
dy102 <ekagnlek2@gmail.com>
2024-02-10 12:33:49 +0900

    fix: 파일 경로 수정

--- server/src/main/java/com/example/thinker/controller/MemberController.java
+++ server/src/main/java/com/example/thinker/controller/MemberController.java
@@ -68,7 +68,7 @@ public class MemberController {
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
         }
-        imageService.makeBasicImage(member, "image/person.jpeg");
+        imageService.makeBasicImage(member, "server/image/person.jpeg");
         return new ResponseEntity<>("success create memberData", HttpStatus.OK);
     }
 
